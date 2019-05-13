dfceee4ac1a825f3621de3a9a06dd017899394c2
Oleh.Bondaruk <bondaruk9000@gmail.com>
2019-05-14 00:26:05 +0300

    draft page added

--- app.js
+++ app.js
@@ -2,43 +2,24 @@ const app = require('express')();
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: '', // TODO: insert your user
-    password: '', // TODO: insert your password
-    database: 'join_us'
+  host: 'localhost',
+  user: '', // TODO: insert your user
+  password: '', // TODO: insert your password
+  database: 'join_us'
 })
 const port = 4000;
-const getUsersCountQuery = `SELECT COUNT(*) FROM users;`
-
-app.get('/', function(req, res){
-  const usersCount = makeQuery(getUsersCountQuery)
-  console.log(usersCount);
-  res.send(`
-  Hello there buddy!
-  You have ${usersCount}
-  `)
+const getEmailsCountQuery = `SELECT COUNT(*) as emails_count FROM emails;`
+app.get('/', function (req, res) {
+  connection.query(getEmailsCountQuery, (err, result, fields) => {
+    if(err) {console.log('Error!!!', err); return err;}
+    const [{emails_count}] = result;
+    res.setHeader("Access-Control-Allow-Origin", true)
+    res.send(`Hello there buddy! You have ${emails_count} Users in your app.`)
+  })
 });
 
-app.get('/joke', function(req, res){
-  res.send(`Wanna joke, huh? Sorry, get back tomorrow, they aren't here yet.`)
-});
-
-app.get('/random', function(req, res){
-  res.send(`Your number is ${Math.floor(Math.random() * 10) + 1}`)
-});
+app.post('/insert_user', (request, response) => {
+  response.sendStatus(200);
+})
 
 app.listen(port, () => console.log(`App is up and running on ${port} port`))
-
-
-function resultCB(err, result, fields) {
-  console.log(err);
-  console.log(result);
-}
-
-function makeQuery(queryString, dataObject) {
-  connection.connect()
-  dataObject
-   ? connection.query(queryString, dataObject, resultCB)
-   : connection.query(queryString, resultCB) 
-  connection.end()
-}
